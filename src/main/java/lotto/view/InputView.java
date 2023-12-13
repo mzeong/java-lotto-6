018b597a9153bd8e8cbc41afef6b399d814e830f
김민정 <mjeong@sookmyung.ac.kr>
2023-12-13 13:58:30 +0900

    feat: 당첨 번호, 보너스 번호를 입력 받는 기능 추가

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -1,12 +1,24 @@
 package lotto.view;
 
 import static lotto.view.Message.INPUT_AMOUNT;
+import static lotto.view.Message.INPUT_BONUS_NUMBER;
+import static lotto.view.Message.INPUT_WINNING_NUMBER;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     public String inputAmount() {
         printMessage(INPUT_AMOUNT);
+        return Console.readLine();
+    }
+
+    public String inputWinningNumber() {
+        printMessage(INPUT_WINNING_NUMBER);
+        return Console.readLine();
+    }
+
+    public String inputBonusNumber() {
+        printMessage(INPUT_BONUS_NUMBER);
         return Console.readLine();
     }
 
