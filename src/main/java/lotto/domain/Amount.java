032afc15310fbc82c2f219a978786d84152b109f
김민정 <mjeong@sookmyung.ac.kr>
2023-12-13 14:30:33 +0900

    feat: 구입 금액이 음수인지 검증하는 기능 추가

--- src/main/java/lotto/domain/Amount.java
+++ src/main/java/lotto/domain/Amount.java
@@ -1,5 +1,6 @@
 package lotto.domain;
 
+import static lotto.util.ErrorMessage.AMOUNT_OUT_OF_RANGE;
 import static lotto.util.ErrorMessage.INVALID_UNIT;
 
 public class Amount {
@@ -7,15 +8,20 @@ public class Amount {
     private final int amount;
 
     public Amount(int amount) {
+        validateUnit(amount);
         validateRange(amount);
         this.amount = amount;
     }
 
-    private void validateRange(int amount) {
+    private void validateUnit(int amount) {
         if (amount % UNIT != 0) {
             throw new IllegalArgumentException(INVALID_UNIT.getMessage());
         }
     }
 
-
+    private void validateRange(int amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException(AMOUNT_OUT_OF_RANGE.getMessage());
+        }
+    }
 }
