dcaa9d94add28ccd5946552d26e370c96f272245
김민정 <mjeong@sookmyung.ac.kr>
2023-12-13 15:32:35 +0900

    feat: 정렬된 중복되지 않는 6개의 숫자로 로또 발행

---
+++ src/main/java/lotto/util/RandomNumberGenerator.java
@@ -0,0 +1,17 @@
+package lotto.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
+public class RandomNumberGenerator {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 45;
+    private static final int COUNT = 6;
+
+    public List<Integer> generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, COUNT);
+        return numbers.stream()
+                .sorted()
+                .toList();
+    }
+}
